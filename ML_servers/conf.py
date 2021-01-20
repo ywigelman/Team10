8f3c7364a1736e77c9b1e377f9d5399b167620b4
Daniella Grimberg <57309621+danigrim@users.noreply.github.com>
2021-01-20 15:56:28 +0200

    Delete conf.py

--- ML_servers/conf.py
+++
@@ -1,9 +0,0 @@
-CLOUDINARY_CLOUD_NAME='hackathonitc'
-CLOUDINARY_API_KEY='897414665579434'
-CLOUDINARY_API_SECRET='Wnb3-tpHWik-oTdE0sdGOzo-wqY'
-# TODO - input the actual values for conversion here
-PREDICTION_TO_LETTER = {
-    0: 'A',
-    1: 'B',
-    2: 'C',
-}
