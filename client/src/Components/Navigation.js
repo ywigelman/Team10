bef918fddc55f1df9c948d82f98196574b8c49b7
NEIL1821 <69971394+NEIL1821@users.noreply.github.com>
2021-01-20 13:49:38 +0200

    main layout with timer component

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Navbar, Nav, Row} from 'react-bootstrap';
+import {Navbar, Nav, Row, Form, Button} from 'react-bootstrap';
+// import { MDBCol,  MDBBtn } from "mdbreact";
+// import 'mdbreact/dist/css/mdb.css'; 
 
 export function Navigation() {
-    return (<Navbar>
-        <Link to='/'>HOME</Link>
-        <Link to='/learningPage'>LEARN NOW</Link>
-        <Link to='/about'>ABOUT</Link>
-        <Link to='/profile'>PROFILE</Link>
-    </Navbar>)
+    return (<Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">ASL APP</Navbar.Brand>
+        <Nav className="mr-auto">
+            <Link to='/' className="mx-2">HOME</Link>
+            <Link to='/learningPage' className="mx-2">LEARN NOW</Link>
+            <Link to='/about' className="mx-2">ABOUT</Link>
+            <Link to='/profile' className="mx-2">PROFILE</Link>
+        </Nav>
+        <Form inline>
+            <Button variant="outline-info">Search</Button>
+        </Form>
+  </Navbar>)
 }
