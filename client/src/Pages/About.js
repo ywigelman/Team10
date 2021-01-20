5f6bbc3e81f6f43b11ae7047e539e615bdef0f3c
julogav <julietgavison@gmail.com>
2021-01-20 21:06:56 +0200

    about page content update

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
 export function About() {
 	return (
 		<Container>
-			<Row>
+			<Row mt-3>
 				<Col></Col>
-				<Col>
-					<Card>
-						<Card.Body>
-							<h2>About Sign&Shoot </h2>
-							<p>
-								Welcome to our community!
-								<br />
-								<i>Sign & Shoot</i> is designed to teach American Sign Language
-								to everyone. <br />
-								Together, we will build a community, and elevate bridges of
-								communication to allow main stream culture to merge with the
-								silent culture. <br />
-								Our main goal is to make Sign Language accessible world-wide
-								with a easy to use app and friendly interface.
-							</p>
-						</Card.Body>
-					</Card>
+				<Col sm={8}>
+					<Jumbotron>
+						<h2>About Sign & Shoot </h2>
+						<p>
+							<br />
+							<h3> Welcome to our community!</h3>
+							<br />
+							<br />
+							<i>Sign & Shoot</i> is designed to teach American Sign Language to
+							everyone. <br />
+							Together, we will build a community, and elevate bridges of
+							communication to allow main stream culture to merge with the
+							silent culture. <br />
+							Our main goal is to make Sign Language accessible world-wide with
+							a easy to use app and friendly interface.
+						</p>
+					</Jumbotron>
 				</Col>
 				<Col></Col>
 			</Row>
