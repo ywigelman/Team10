04f0b29e0f6106500ff4c053cf292e20ff3df59b
danigrim <danigrim@stanford.edu>
2021-01-20 15:51:12 +0200

    feature-skeleton_inf_server

--- ML_servers/inference.py
+++ ML_servers/inference.py
@@ -1,36 +1,54 @@
 import sys
-
+import numpy as np
+import cv2, wget
+import cloudinary.api
 from flask import Flask, request, jsonify
 import pickle
-import pandas as pd
-import json
 import os
+import cv2
+import cloudinary
+from conf import CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, PREDICTION_TO_LETTER
 
+# TODO - Download model here
 filename = 'model_name'
 app = Flask(__name__)
 model = pickle.load(open(filename,'rb'))
 
+cloudinary.config(
+  cloud_name = CLOUDINARY_CLOUD_NAME,
+  api_key = CLOUDINARY_API_KEY,
+  api_secret = CLOUDINARY_API_SECRET
+)
 
-@app.route('/predict_single', methods=['GET'])
-def predict_single():
+
+@app.route('/image_predict', methods=['GET'])
+def predict_sign():
+    # TODO- test this out with real model
     try:
-        df = pd.DataFrame.from_dict({feat: [float(request.args.get(feat))] for feat in request.args})
-        prediction = model.predict(df)[0]
-    except:
-        return "URL not properly formatted. Try format: /predict_single?age=10&sex=1&trestbps=140&chol=193&fbs=0.0&restecg=0.0&thalach=145&exang=1.0&oldpeak=1.0&slope=2.0"
+        im = cloudinary.api.resource(request.args.get("public_id"))
+        im_file = wget.download(im['url'])
+        im_arr = cv2.resize(cv2.imread(im_file), (224, 224))
+        prediction = PREDICTION_TO_LETTER[model.predict(im_arr)]
+    except Exception as e:
+        return f"Error: {e}"
     return str(prediction)
 
 
-@app.route('/predict_many', methods=['POST'])
-def predict_many():
+@app.route('/multi_image_predict', methods=['GET'])
+def predict_sign_multi():
+    # TODO - Try with actual model, validate request args
     try:
-        content = json.loads(request.get_json())
-        df = pd.DataFrame(content)
-        prediction = model.predict(df)
+        predictions = np.array([0 for _ in range(len(PREDICTION_TO_LETTER.keys()))])
+        for im_id in request.args.get("public_ids"):
+            im = cloudinary.api.resource(im_id)
+            im_file = wget.download(im['url'])
+            im_arr = cv2.resize(cv2.imread(im_file), (224, 224))
+            prediction = model.predict(im_arr)
+            predictions[prediction] += 1
+        prediction = PREDICTION_TO_LETTER[np.argmax(predictions)]
     except Exception as e:
-         return jsonify({'error':"JSON wrong format" })
-    lists = prediction.tolist()
-    return json.dumps(lists)
+         return f"Error: {e}"
+    return str(prediction)
 
 
 if __name__ == "main":
