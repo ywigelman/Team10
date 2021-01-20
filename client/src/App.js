688361be0e2e71aa59771455046f8784025a4ee7
Idan Amati <idan.amati@gmail.com>
2021-01-20 11:52:15 +0200

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-import WebcamCapture from './Components/WebcamCapture'
+import WebcamCapture from './Components/WebcamCapture';
+import React, { useState } from 'react';
 
 function App() {
+  const [camera, setCamera] = useState(false)
+  const [imgSrc, setImgSrc] = React.useState([]);
+
+  const processPhotos = (photo) => {
+    setImgSrc([photo, ...imgSrc]);
+  }
+
   return (
     <div className="App">
-  <WebcamCapture/>      
+      <WebcamCapture active={camera} photoProcessor={processPhotos}/>      
+      <button onClick={() => setCamera(!camera)}>toggle camera</button>
+      <div>
+          {imgSrc.map((item, i) => (
+            <img src={item} key={i} alt={i} />
+          ))}
+        </div>
     </div>
   );
 }
--- client/src/Components/WebcamCapture.jsx
+++ client/src/Components/WebcamCapture.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Webcam from "react-webcam";
 
-const WebcamCapture = () => {
+const WebcamCapture = ({ active, photoProcessor }) => {
   const [deviceId, setDeviceId] = React.useState({});
   const [cameraLoader, setCameraLoader] = React.useState(true);
   const [devices, setDevices] = React.useState([]);
-  const [imgSrc, setImgSrc] = React.useState([]);
 
   const webcamRef = React.useRef(null);
 
@@ -14,8 +13,9 @@ const WebcamCapture = () => {
       height: 240,
       width: 320,
     });
-    setImgSrc((images) => [imageSrc, ...images]);
-  }, [webcamRef, setImgSrc]);
+    console.log(webcamRef.current.getScreenshot());
+    photoProcessor(imageSrc);
+  }, [webcamRef, photoProcessor]);
 
   const captureMany = (many) => {
     let count = 1;
@@ -38,36 +38,36 @@ const WebcamCapture = () => {
     navigator.mediaDevices.enumerateDevices().then(handleDevices);
   }, [handleDevices]);
 
-  return (
-    <>
-      {cameraLoader && "Allow camera to start taking pictures"}
-      <Webcam
-        ref={webcamRef}
-        audio={false}
-        videoConstraints={{ deviceId }}
-        onUserMedia={() => setCameraLoader(false)}
-      />
-      <div>
-        {devices.map((device, key) => (
-          <button
-            key={device.deviceId}
-            onClick={() => setDeviceId(device.deviceId)}
-          >
-            {device.label || `Device ${key + 1}`}
-          </button>
-        ))}
-      </div>
-      <div>
-        <button onClick={capture}>take one pic</button>
-        <button onClick={() => captureMany(10)}>take 10 pictures</button>
-      </div>
-      <div>
-        {imgSrc.map((item, i) => (
-          <img src={item} key={i} alt={i} />
-        ))}
-      </div>
-    </>
-  );
+  if (active)
+    return (
+      <>
+        {cameraLoader && "Allow camera to start taking pictures"}
+        {active && (
+          <Webcam
+            ref={webcamRef}
+            audio={false}
+            videoConstraints={{ deviceId }}
+            onUserMedia={() => setCameraLoader(false)}
+          />
+        )}
+        <div>
+          {devices.map((device, key) => (
+            <button
+              key={device.deviceId}
+              onClick={() => setDeviceId(device.deviceId)}
+            >
+              {device.label || `Device ${key + 1}`}
+            </button>
+          ))}
+        </div>
+        <div>
+          <button onClick={capture}>take one pic</button>
+          <button onClick={() => captureMany(10)}>take 10 pictures</button>
+        </div>
+      </>
+    );
+
+  return <div>Camera not active</div>;
 };
 
 export default WebcamCapture;
