b005e8d822f6f316df82e58b0df58ca4c35a39da
Sagiy <sagi88@gmail.com>
2021-01-20 22:33:53 +0200

    added name field and done button

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Dictionary } from '../Components/dictionary';
+import React from "react";
+import { Card, InputGroup, FormControl, Button } from "react-bootstrap";
 
 export function Home() {
-
-    return (<>
-        <h1>Home Page</h1>
-    </>)
+  return (
+    <Card>
+      <h1>Welcome to Sign&Shoot</h1>
+      <InputGroup>
+        <FormControl
+          placeholder="Please Enter your Name"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Append>
+          <Button variant="outline-secondary">Done</Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </Card>
+  );
 }
