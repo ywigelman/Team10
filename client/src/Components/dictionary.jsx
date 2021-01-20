589238503df090c0af35c7c26fe7940c053f2709
julogav <julietgavison@gmail.com>
2021-01-20 16:59:42 +0200

    flippin' cards

--- client/src/Components/dictionary.jsx
+++ client/src/Components/dictionary.jsx
@@ -1,17 +1,43 @@
-import { Card, CardDeck } from 'react-bootstrap';
+import { Card, CardColumns, Container, Row } from 'react-bootstrap';
 import alphabet from './abc-pics';
+import ReactCardFlip from 'react-card-flip';
+import { useState } from 'react';
+import { Button } from 'bootstrap';
 
 export const Dictionary = () => {
+	const [isFlipped, setIsFlipped] = useState(false);
+
+	const onClick = e => {
+		e.preventDefault();
+		if (!isFlipped) setIsFlipped(true);
+		if (isFlipped) setIsFlipped(false);
+	};
+
 	return (
-		<CardDeck>
-			{alphabet &&
-				alphabet.map(({ id, src, title, alt }) => (
-					<Card key={id}>
-						<Card.Img src={src} title={title} alt={alt} />
-					</Card>
-				))}
-		</CardDeck>
+		<Container>
+			<CardColumns className='m-3'>
+				{alphabet &&
+					alphabet.map(({ id, src, title, alt }) => (
+						<ReactCardFlip isFlipped={!isFlipped}>
+							<Card border='info' className='text-center' key={id}>
+								<Card.Img
+									variant='top'
+									fluid
+									src={src}
+									title={title}
+									alt={alt}
+								/>
+								<Card.Body>Click to see the letter</Card.Body>
+								<Button onClick={onClick}>Flip</Button>
+							</Card>
+
+							<Card>
+								<h1>{id}</h1>
+								<Button onClick={onClick}>Flip</Button>
+							</Card>
+						</ReactCardFlip>
+					))}
+			</CardColumns>
+		</Container>
 	);
 };
-
-
