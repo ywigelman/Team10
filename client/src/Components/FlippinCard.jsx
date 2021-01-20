e9590428248a2be3a6ba7d453d9d48ccccddef46
Idan Amati <idan.amati@gmail.com>
2021-01-20 19:38:40 +0200

    flipping card

---
+++ client/src/Components/FlippinCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export default function FlippingCard({ children, otherSide }) {
+  const [flipped, setFlipped] = React.useState(false);
+  return (
+    <div
+      onClick={() => setFlipped(!flipped)}
+      style={{ position: "relative", cursor: "pointer" }}
+    >
+      <div style={{ opacity: "0" }}>
+        <div style={{ opacity: "0" }}>{children}</div>
+      </div>
+      <Card
+        style={{
+          position: "absolute",
+          top: 0,
+          width: "100%",
+          height: "100%",
+          transition: "transform 0.8s",
+          transform: flipped ? "rotateY(180deg)" : "rotateY(0deg)",
+          transformStyle: "preserve-3d",
+          "-webkit-transform-style": "preserve-3d",
+          backfaceVisibility: "hidden",
+        }}
+      >
+        {children}
+      </Card>
+      <Card
+        style={{
+          position: "absolute",
+          top: 0,
+          width: "100%",
+          height: "100%",
+          transition: "transform 0.8s",
+          transform: flipped ? "rotateY(0deg)" : "rotateY(-180deg)",
+          transformStyle: "preserve-3d",
+          "-webkit-transform-style": "preserve-3d",
+          backfaceVisibility: "hidden",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {otherSide}
+      </Card>
+    </div>
+  );
+}
