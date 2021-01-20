7836dad5beb8cc799a4f2c86536101fc1d752651
Sagiy <sagi88@gmail.com>
2021-01-20 14:27:56 +0200

    some about us info

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,8 +1,23 @@
-import React from 'react';
-
+import React from "react";
+import { Card, Container, Row, Col } from "react-bootstrap";
 export function About() {
-
-    return (
-        <h1>About Page</h1>
-    )
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <h2>About Sign&Shoot </h2>
+              <p>
+                Our Main goal is to make Sign Language accessible world-wide
+                with a easy to use app and friendly interface{" "}
+              </p>{" "}
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 }
