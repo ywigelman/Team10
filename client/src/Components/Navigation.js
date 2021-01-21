9a17bb85ce85332309c3060a56ef530662b4f55c
Idan Amati <idan.amati@gmail.com>
2021-01-21 12:08:02 +0200

    navigation play icon

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -17,11 +17,12 @@ export function Navigation() {
         {/* <Navbar.Brand href="/">LOGO</Navbar.Brand> */}
         <Nav className="d-flex justify-content-around py-2 container">
             <Link to='/' className="d-flex flex-column align-items-center" style={{color:'#F8F8F8'}}>
-                <FaHome size="2rem"/>
+                    <FaHome size="2rem"/>
                 <div className="d-none d-md-block" style={{fontWeight:'bold'}}>HOME</div>
             </Link>
             <Link to='/learningPage' className="d-flex flex-column align-items-center" style={{color:'#F8F8F8'}}>
-                <FaGamepad size="2rem" />
+                <img style={{height:"2rem"}} src="/Logos-White.svg"/>
+                {/* <FaGamepad size="2rem" /> */}
                 <div  className="d-none d-md-block"  style={{fontWeight:'bold'}}>PLAY</div>
             </Link>
             <Link to='/about' className="d-flex flex-column align-items-center" style={{color:'#F8F8F8'}}>
