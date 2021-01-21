2bcf9a8eed6a0ca74b22506482d0d913364b852a
Idan Amati <idan.amati@gmail.com>
2021-01-21 11:18:51 +0200

    💅 navbar style

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -1,49 +1,40 @@
 import React, {useState} from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import {Navbar, Nav, Form, Button} from 'react-bootstrap';
-// import { MDBCol,  MDBBtn } from "mdbreact";
-// import 'mdbreact/dist/css/mdb.css'; 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {FaGamepad, FaHome, FaInfo, FaBook, FaUser} from 'react-icons/fa'
+
 import { MyVerticallyCenteredModal } from './ModalDictionary';
 
 
 export function Navigation() {
     const [modalShow, setModalShow] = useState(false);
 
-    const hoverDictionary = (e) => {
-        e.target.style.color ="blue";
-    };
-
-    const exitHover = (e) => {
-        e.target.style.color ="#F8F8F8";
-    };
-
     return (<>
     {/* <Form inline className="fixed-bottom" style={{marginBottom:"5rem"}}>
         <Button variant="outline-info">Dictionary</Button>
     </Form> */}
-    <Navbar style={{backgroundColor:'#6800F4', color:'white'}} fixed="bottom">
+    <Navbar style={{backgroundColor:'#6800F4', color:'white'}} fixed="bottom" >
         {/* <Navbar.Brand href="/">LOGO</Navbar.Brand> */}
-        <Nav className="mr-auto">
-            <Link to='/' className="mx-sm-5 mx-1" style={{color:'#F8F8F8'}}>
-                <FontAwesomeIcon icon="home" className="fa-2x" />
-                <div style={{fontWeight:'bold'}}>HOME</div>
+        <Nav className="d-flex justify-content-around py-2 container">
+            <Link to='/' className="d-flex flex-column align-items-center" style={{color:'#F8F8F8'}}>
+                <FaHome size="2rem"/>
+                <div className="d-none d-md-block" style={{fontWeight:'bold'}}>HOME</div>
             </Link>
-            <Link to='/learningPage' className="mx-sm-5 mx-1" style={{color:'#F8F8F8'}}>
-                <FontAwesomeIcon icon="gamepad" className="fa-2x" />
-                <div style={{fontWeight:'bold'}}>PLAY</div>
+            <Link to='/learningPage' className="d-flex flex-column align-items-center" style={{color:'#F8F8F8'}}>
+                <FaGamepad size="2rem" />
+                <div  className="d-none d-md-block"  style={{fontWeight:'bold'}}>PLAY</div>
             </Link>
-            <Link to='/about' className="mx-sm-5 mx-1" style={{color:'#F8F8F8'}}>
-                <FontAwesomeIcon icon="info" className="fa-2x ml-4" />
-                <div style={{fontWeight:'bold'}}>ABOUT</div>
+            <Link to='/about' className="d-flex flex-column align-items-center" style={{color:'#F8F8F8'}}>
+                <FaInfo size="2rem" />
+                <div  className="d-none d-md-block"  style={{fontWeight:'bold'}}>ABOUT</div>
             </Link>
-            <Link to='/profile' className="mx-sm-5 mx-1" style={{color:'#F8F8F8'}}>
-                <FontAwesomeIcon icon="user" className="fa-2x ml-3" />
-                <div style={{fontWeight:'bold'}}>PROFILE</div>
+            <Link to='/profile' className="d-flex flex-column align-items-center" style={{color:'#F8F8F8'}}>
+                <FaUser size="2rem"/>
+                <div  className="d-none d-md-block"  style={{fontWeight:'bold'}}>PROFILE</div>
             </Link>
-            <div onMouseOver={(e)=> hoverDictionary(e)} onMouseOut={(e) => exitHover(e)} onClick={() => setModalShow(true)}>
-                <FontAwesomeIcon icon="book" className="fa-2x ml-4" style={{color:'#F8F8F8'}}/>
-                <div style={{fontWeight:'bold'}}>DICTIONARY</div>
+            <div className="d-flex flex-column align-items-center" onClick={() => setModalShow(true)} style={{cursor:'pointer'}}>
+                <FaBook size="2rem"/>
+                <div  className="d-none d-md-block"  style={{fontWeight:'bold'}}>DICT</div>
             </div>
         </Nav>  
   </Navbar>
